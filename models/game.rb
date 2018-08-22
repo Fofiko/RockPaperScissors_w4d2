a77cdbd0c6eb883efeb5870b1da0079fb15c7cbe
Fofiko <foteinid@gmail>
2018-08-22 07:56:20 +0100

    Updated model to indicate both winner and weapon and use a case statement

--- models/game.rb
+++ models/game.rb
@@ -5,19 +5,28 @@ class Game
     @player2 = player2
   end
 
+  # def find_winner()
+  #   if (@player1 == 'rock' && @player2 == 'paper') || (@player1 == 'paper' && @player2 == 'rock')
+  #     return "Paper beats rock."
+  #   elsif (@player1 == 'rock' && @player2 == 'scissors') || (@player1 == 'scissors' && @player2 == 'rock')
+  #     return "Rock beats scissors."
+  #   elsif (@player1 == 'paper' && @player2 == 'scissors') || (@player1 == 'scissors' && @player2 == 'paper')
+  #     return "Scissors beats paper."
+  #   else
+  #     return "It's a tie!"
+  #   end
+  # end
+
   def find_winner()
-    if (@player1 == 'rock' && @player2 == 'paper') || (@player1 == 'paper' && @player2 == 'rock')
-      return "Paper beats rock."
-    elsif (@player1 == 'rock' && @player2 == 'scissors') || (@player1 == 'scissors' && @player2 == 'rock')
-      return "Rock beats scissors."
-    elsif (@player1 == 'paper' && @player2 == 'scissors') || (@player1 == 'scissors' && @player2 == 'paper')
-      return "Scissors beats paper."
-    else
+    case
+    when @player1 == 'rock' && @player2 == 'scissors' || @player1 == 'scissors' && @player2 == 'paper'
+      return "Player1 chose #{@player1} and beats Player2's #{@player2} "
+    when @player1 == @player2
       return "It's a tie!"
+    else
+      return "Player1 chose #{@player1} and looses against Player2's #{@player2} "
     end
   end
-
-
 
 
 end
